bb034bd37e4e74bbf1f0f015dd49b687a78c6034
martijnv <martijnvoordouw@gmail.com>
2023-08-19 11:37:52 +0200

    shells bounce of the side off the screen, and they now have a sprite

--- flight/shell.py
+++ flight/shell.py
@@ -15,16 +15,27 @@ def update_shells(plane):
         shells.append(bullet.create_bullet(plane))
     for shell in shells:
         bullet.move_bullet(shell)
-    shell = [shell for shell in shells if not is_offscreen(shell.x, shell.y)]
+        if is_offscreen(shell.x, shell.y):
+            bounce_shell(shell, shells)
+    
 
 
 def draw_shells():
     for shell in shells:
-        ellipse((0,255,0), shell.x, shell.y, 20, 20)
+        image("flight/koopa_shell.png", shell.x, shell.y, 2, 0)
 
+
+def bounce_shell(shell, shells):
+    shell.direction = bounce_angle(shell.direction)
+    try:
+        shell.bounce_count -= 1
+        if shell.bounce_count < 0:
+            shells.remove(shell)
+    except AttributeError:
+        shell.bounce_count = 4
 
 def shell_collisions(ghosts):
-    shell_radius = 10
+    shell_radius = 15
     # for simplicitys sake we threat the ghosts as circles during intersections
     ghost_radius = 28.5 
     for shell in shells:
@@ -32,4 +43,4 @@ def shell_collisions(ghosts):
             d = distance(ghost.x, ghost.y, shell.x, shell.y)
             if d < shell_radius + ghost_radius:
                 ghosts.remove(ghost)
-                shell.direction = bounce_angle(shell.direction)
+                bounce_shell(shell, shells)
